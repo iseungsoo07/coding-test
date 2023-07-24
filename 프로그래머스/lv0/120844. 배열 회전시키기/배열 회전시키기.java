0edd224d1a0a47fea6f1c1c6e6603fefa52beb26
SeungSoo Song <iseungsoo07@gmail.com>
2023-07-24 13:15:29 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.17 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,17 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers, String direction) {
         int[] answer = new int[numbers.length];
         
-        if(direction.equals("left")) {
-           for(int i = 0; i < answer.length - 1; i++) {
-               answer[i] = numbers[i + 1];
-           }
-           answer[answer.length - 1] = numbers[0];
-        } else if(direction.equals("right")) {
-            for(int i = 1; i < answer.length; i++) {
-               answer[i] = numbers[i - 1];
-           }
-           answer[0] = numbers[numbers.length - 1];
+        Deque<Integer> deque = new LinkedList<>();
+        
+        for(int item : numbers) {
+            deque.addLast(item);
+        }
+        
+        if("right".equals(direction)) {
+            int data = deque.removeLast();
+            deque.addFirst(data);
+        } else if ("left".equals(direction)) {
+            int data = deque.removeFirst();
+            deque.addLast(data);
+        }
+        
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = deque.removeFirst();
         }
         
         return answer;
