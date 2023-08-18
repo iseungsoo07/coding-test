5e90bf0e0dee2411c8c7936eb0f149b689c62869
SeungSoo Song <iseungsoo07@gmail.com>
2023-08-18 21:26:22 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.17 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        
+        long[] dp = new long[n + 1];
+        
+        if(n == 1) {
+            return 1;
+        }
+        
+        if(n == 2) {
+            return 2;
+        }
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}
