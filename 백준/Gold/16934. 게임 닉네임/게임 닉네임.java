4e5308479334ad259e511604f6ed12d699531a2e
SeungSoo Song <iseungsoo07@gmail.com>
2023-10-11 17:23:29 +0900

    [Gold III] Title: 게임 닉네임, Time: 1044 ms, Memory: 119516 KB -BaekjoonHub

---
+++ 백준/Gold/16934. 게임 닉네임/게임 닉네임.java
@@ -0,0 +1,64 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static Map<String, Integer> cntMap = new HashMap<>();
+
+    static class Node {
+        Map<Character, Node> child = new HashMap<>();
+    }
+
+    static class Trie {
+        Node root;
+
+        public Trie() {
+            this.root = new Node();
+        }
+
+        public String insert(String str) {
+            Node cur = root;
+            boolean flag = false;
+            int idx = str.length();
+
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+
+                if (!cur.child.containsKey(c)) {
+                    cur.child.put(c, new Node());
+
+                    if (!flag) {
+                        idx = i + 1;
+                        flag = true;
+                    }
+                }
+
+                cur = cur.child.get(c);
+            }
+
+            if (!cntMap.containsKey(str)) {
+                cntMap.put(str, 1);
+                return str.substring(0, idx);
+            } else {
+                cntMap.put(str, cntMap.get(str) + 1);
+                return str.substring(0, idx).concat(String.valueOf(cntMap.get(str)));
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Trie trie = new Trie();
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            sb.append(trie.insert(sc.next())).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
