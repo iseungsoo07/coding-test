c61db073cdb4545385abbabc5c0a058deadc658b
SeungSoo Song <iseungsoo07@gmail.com>
2024-02-09 18:19:45 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,34 +1,40 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main (String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int count = 0;
-        ArrayList<Character> arrList = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
+		int N = Integer.parseInt(st.nextToken());
 
-            count++;
-            for (int j = 0; j < str.length() - 1; j++) {
-                if (str.charAt(j) != str.charAt(j + 1)) {
-                    if (arrList.contains(str.charAt(j)) || arrList.contains(str.charAt(j + 1))) {
-                        count--;
-                        break;
-                    }
-                    arrList.add(str.charAt(j));
-                }
-            }
-            arrList.clear();
-        }
+		for (int i = 0; i < N; i++) {
+			int[] alphabet = new int[26];
+			boolean groupCheck = true;
 
-        br.close();
-        System.out.println(count);
-    }
+			st = new StringTokenizer(br.readLine());
+
+			String str = st.nextToken();
+			alphabet[str.charAt(0) - 'a']++;
+			for (int j = 1; j < str.length(); j++) {
+				if (alphabet[str.charAt(j) - 'a'] != 0 && str.charAt(j) != str.charAt(j - 1)) {
+					groupCheck = false;
+					break;
+				}
+
+				alphabet[str.charAt(j) - 'a']++;
+			}
+
+			if (groupCheck) {
+				count++;
+			}
+		}
+
+		System.out.println(count);
+	}
+
 }
