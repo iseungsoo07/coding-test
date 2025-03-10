de694cb6588c21a6f6e0128f755fd9b28b6335a9
SeungSoo Song <iseungsoo07@gmail.com>
2025-03-10 21:06:47 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1840 ms, Memory: 73624 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[][] board;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nX = cur[0] + dx[i];
+                int nY = cur[1] + dy[i];
+
+                if (nX < 0 || nX >= N || nY < 0 || nY >= M) {
+                    continue;
+                }
+
+                if (board[nX][nY] == 0) {
+                    continue;
+                }
+
+                if (visited[nX][nY]) {
+                    continue;
+                }
+
+                queue.offer(new int[]{nX, nY});
+                visited[nX][nY] = true;
+                board[nX][nY] = board[cur[0]][cur[1]] + 1;
+            }
+        }
+    }
+
+    public static int[] findDest() {
+        int x = 0, y = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (board[i][j] == 2) {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        return new int[]{x, y};
+    }
+
+    public static boolean check(int x, int y, int[] dest) {
+        int destX = dest[0];
+        int destY = dest[1];
+
+        if ((x == destX + 1 && y == destY) || (x == destX - 1 && y == destY) || (x == destX
+            && y == destY + 1) || (x == destX && y == destY - 1)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[] dest = findDest();
+
+        board[dest[0]][dest[1]] = 0;
+        bfs(dest[0], dest[1]);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (board[i][j] == 1) {
+                    if (check(i, j, dest)) {
+                        board[i][j] = -1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
