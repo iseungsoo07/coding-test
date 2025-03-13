db827ea38232519a58c72fe583a9b34560aa1afc
SeungSoo Song <iseungsoo07@gmail.com>
2025-03-13 20:54:21 +0900

    [Gold V] Title: AC, Time: 704 ms, Memory: 77756 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -2,91 +2,73 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static Deque<Integer> initDeque(String[] str, Deque<Integer> deque, Deque<Integer> reverse_deque) {
-        Deque<Integer> deq = new ArrayDeque<>();
-
-        for (String s : str) {
-            if (s.equals("")) {
-                break;
-            }
-
-            deque.addLast(Integer.parseInt(s));
-            reverse_deque.addFirst(Integer.parseInt(s));
-        }
-
-        return deque; // 맨 처음에는 기본 상태
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < T; i++) {
-            String p = br.readLine();
-            int n = Integer.parseInt(br.readLine());
-            String[] str = br.readLine().replace("[", "").replace("]", "").split(",");
 
-            Deque<Integer> deque = new ArrayDeque<>(); // 처음 입력 받은 배열을 저장할 데크
-            Deque<Integer> reverse_deque = new ArrayDeque<>(); // 처음 입력 받은 배열을 역순으로 저장한 데크
-            Deque<Integer> deq = new ArrayDeque<>(); // 연산에 사용할 데크
 
-            deq = initDeque(str, deque, reverse_deque);
-            boolean isError = false;
+        for (int i = 0; i < n; i++) {
+            String func = br.readLine();
+            int count = Integer.parseInt(br.readLine());
+            String arrayInput = br.readLine();
+            String[] arr =
+                arrayInput.equals("[]") ? new String[0] : arrayInput.replace("[", "").replace("]", "").split(",");
 
-            for (int j = 0; j < p.length(); j++) {
-                if (p.charAt(j) == 'R') {
-                    // if (deq.isEmpty()) {
-                    //     sb.append("[").append("]");
-                    //     break;
-                    // }
 
-                    if (deq == deque) {
-                        deq = reverse_deque;
-                    } else {
-                        deq = deque;
-                    }
-                } else if (p.charAt(j) == 'D') {
-                    if (deq.isEmpty()) {
-                        isError = true;
-                        sb.append("error");
+            Deque<String> deque = new ArrayDeque<>(Arrays.asList(arr));
+
+            boolean errorCheck = false;
+            boolean front = true;
+
+            for (int j = 0; j < func.length(); j++) {
+                char ch = func.charAt(j);
+
+                if (ch == 'R') {
+                    front = !front;
+                } else if (ch == 'D') {
+                    if (deque.isEmpty()) {
+                        errorCheck = true;
+                        sb.append("error\n");
                         break;
                     }
 
-                    deq.pollFirst();
-                    if (deq == deque) {
-                        reverse_deque.pollLast();
+                    if (front) {
+                        deque.pollFirst();
                     } else {
                         deque.pollLast();
                     }
                 }
             }
 
-            if (!deq.isEmpty()) {
+            if (!errorCheck) {
                 sb.append("[");
-                for (Integer data : deq) {
-                    sb.append(data).append(",");
-                }
-                sb.deleteCharAt(sb.length() - 1);
-                sb.append("]");
-            } else if (!isError) {
-                sb.append("[").append("]");
-            }
 
-            if (i != T - 1) {
-                sb.append("\n");
+                while (!deque.isEmpty()) {
+                    if (front) {
+                        sb.append(deque.pollFirst());
+                    } else {
+                        sb.append(deque.pollLast());
+                    }
+
+                    if (!deque.isEmpty()) {
+                        sb.append(",");
+                    }
+                }
+
+                sb.append("]\n");
             }
-            isError = false;
         }
 
-        br.close();
-        System.out.print(sb);
-
+        System.out.println(sb);
     }
-
-
 }
