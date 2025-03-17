791115fc4fc78aa5086b91f136c15fbfff297103
SeungSoo Song <iseungsoo07@gmail.com>
2025-03-17 19:57:07 +0900

    [Silver IV] Title: 수 찾기, Time: 684 ms, Memory: 43824 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,31 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
+        int[] A = new int[n];
 
-        String[] A = br.readLine().split(" ");
-
-        HashSet<Integer> set = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            set.add(Integer.parseInt(A[i]));
+            A[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(A);
 
         int m = Integer.parseInt(br.readLine());
-        String[] M = br.readLine().split(" ");
+        int[] M = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            M[i] = Integer.parseInt(st.nextToken());
+        }
 
         for (int i = 0; i < m; i++) {
-            if (set.contains(Integer.parseInt(M[i]))) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
+            int left = 0;
+            int right = A.length - 1;
+            boolean flag = false;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (M[i] == A[mid]) {
+                    sb.append(1).append("\n");
+                    flag = true;
+                    break;
+                } else if (M[i] < A[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            if (!flag) {
+                sb.append(0).append("\n");
             }
         }
+
+        System.out.println(sb);
+
     }
 }
