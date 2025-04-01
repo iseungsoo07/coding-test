c3cdbbc85c891d3a71b5ffa9a5fb515c6a1e0131
SeungSoo Song <iseungsoo07@gmail.com>
2025-04-01 14:01:53 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 676 ms, Memory: 105844 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> nameMap = new HashMap<>();
+        Map<Integer, String> numMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String name = br.readLine();
+            nameMap.putIfAbsent(name, i + 1);
+            numMap.putIfAbsent(i + 1, name);
+        }
+
+        for (int i = 0; i < m; i++) {
+            String str = br.readLine();
+
+            try {
+                int num = Integer.parseInt(str);
+
+                sb.append(numMap.get(num)).append("\n");
+            } catch (Exception e) {
+                sb.append(nameMap.get(str)).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
