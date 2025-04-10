18bc56c99ca07eb820193ae343204cf5e5af269c
SeungSoo Song <iseungsoo07@gmail.com>
2025-04-10 13:51:32 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18116 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,25 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int x = sc.nextInt();
-        int[] dp = new int[x + 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 2; i <= x; i++) {
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+
+        if (n == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + 1;
 
             if (i % 2 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
-            
+
             if (i % 3 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
         }
 
-        System.out.println(dp[x]);
-
+        System.out.println(dp[n]);
     }
 }
