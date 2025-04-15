7c7a6d3d33a26a04017bb9ead502f2bccd8873e7
SeungSoo Song <iseungsoo07@gmail.com>
2025-04-15 15:12:16 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,118 +1,121 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Node {
-    String data;
-    Node left;
-    Node right;
-
-    public Node(String data, Node left, Node right) {
-        this.data = data;
-        this.left = left;
-        this.right = right;
-    }
-}
-
-class BinaryTree {
-    Node head;
-
-    public BinaryTree() {
-        this.head = null;
-    }
-
-    public void addNode(String parent, String left, String right) {
-        if (head == null) {
-            head = new Node(parent, null, null);
-            if (".".equals(left)) {
-                head.left = null;
-            } else {
-                head.left = new Node(left, null, null);
-            }
-
-            if (".".equals(right)) {
-                head.right = null;
-            } else {
-                head.right = new Node(right, null, null);
-            }
-        } else {
-            Node node = getNode(parent);
-            if (".".equals(left)) {
-                node.left = null;
-            } else {
-                node.left = new Node(left, null, null);
-            }
-
-            if (".".equals(right)) {
-                node.right = null;
-            } else {
-                node.right = new Node(right, null, null);
-            }
-        }
-    }
-
-    public Node getNode(String s) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(head);
-
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            if (cur.data.equals(s)) {
-                return cur;
-            }
-
-            if (cur.left != null) {
-                queue.offer(cur.left);
-            }
-
-            if (cur.right != null) {
-                queue.offer(cur.right);
-            }
-        }
-
-        return null;
-    }
-
-    public void preOrder(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        System.out.print(node.data);
-        preOrder(node.left);
-        preOrder(node.right);
-    }
-
-    public void inOrder(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        inOrder(node.left);
-        System.out.print(node.data);
-        inOrder(node.right);
-    }
-
-    public void postOrder(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.print(node.data);
-    }
-}
-
 public class Main {
+
+    public static class Node {
+
+        String data;
+        Node left;
+        Node right;
+
+        public Node(String data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class BinaryTree {
+
+        Node head;
+
+        public BinaryTree() {
+            this.head = null;
+        }
+
+        public void addNode(String parent, String left, String right) {
+            if (head == null) {
+                head = new Node(parent, null, null);
+                if (".".equals(left)) {
+                    head.left = null;
+                } else {
+                    head.left = new Node(left, null, null);
+                }
+
+                if (".".equals(right)) {
+                    head.right = null;
+                } else {
+                    head.right = new Node(right, null, null);
+                }
+            } else {
+                Node node = getNode(parent);
+                if (".".equals(left)) {
+                    node.left = null;
+                } else {
+                    node.left = new Node(left, null, null);
+                }
+
+                if (".".equals(right)) {
+                    node.right = null;
+                } else {
+                    node.right = new Node(right, null, null);
+                }
+            }
+        }
+
+        public Node getNode(String s) {
+            Queue<Node> queue = new LinkedList<>();
+            queue.offer(head);
+
+            while (!queue.isEmpty()) {
+                Node cur = queue.poll();
+                if (cur.data.equals(s)) {
+                    return cur;
+                }
+
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+
+            return null;
+        }
+
+        public void preOrder(Node node) {
+            if (node == null) {
+                return;
+            }
+
+            System.out.print(node.data);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+
+        public void inOrder(Node node) {
+            if (node == null) {
+                return;
+            }
+
+            inOrder(node.left);
+            System.out.print(node.data);
+            inOrder(node.right);
+        }
+
+        public void postOrder(Node node) {
+            if (node == null) {
+                return;
+            }
+
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.print(node.data);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-
-        // 노드의 개수
+        StringTokenizer st;
+        
         int nodes = Integer.parseInt(br.readLine());
 
         BinaryTree bt = new BinaryTree();
