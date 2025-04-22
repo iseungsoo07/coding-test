dd6339234814f194565e74bd7a352dee17a53243
SeungSoo Song <iseungsoo07@gmail.com>
2025-04-22 09:56:22 +0900

    [Gold V] Title: 내려가기, Time: 280 ms, Memory: 40104 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,49 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
         int[] maxDp = new int[3];
         int[] minDp = new int[3];
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int x3 = Integer.parseInt(st.nextToken());
 
             if (i == 0) {
-                maxDp[0] = a;
-                minDp[0] = a;
-
-                maxDp[1] = b;
-                minDp[1] = b;
-
-                maxDp[2] = c;
-                minDp[2] = c;
+                maxDp[0] = minDp[0] = x1;
+                maxDp[1] = minDp[1] = x2;
+                maxDp[2] = minDp[2] = x3;
             } else {
-                int prev0 = maxDp[0];
-                int prev2 = maxDp[2];
-                maxDp[0] = Math.max(maxDp[0], maxDp[1]) + a;
-                maxDp[2] = Math.max(maxDp[1], maxDp[2]) + c;
-                maxDp[1] = Math.max(prev0, Math.max(maxDp[1], prev2)) + b;
+                int prevMaxDp0 = maxDp[0];
+                int prevMaxDp2 = maxDp[2];
 
-                prev0 = minDp[0];
-                prev2 = minDp[2];
-                minDp[0] = Math.min(minDp[0], minDp[1]) + a;
-                minDp[2] = Math.min(minDp[1], minDp[2]) + c;
-                minDp[1] = Math.min(prev0, Math.min(minDp[1], prev2)) + b;
+                maxDp[0] = Math.max(maxDp[0], maxDp[1]) + x1;
+                maxDp[2] = Math.max(maxDp[1], maxDp[2]) + x3;
+                maxDp[1] = Math.max(Math.max(prevMaxDp0, maxDp[1]), prevMaxDp2) + x2;
+
+                int prevMinDp0 = minDp[0];
+                int prevMinDp2 = minDp[2];
+
+                minDp[0] = Math.min(minDp[0], minDp[1]) + x1;
+                minDp[2] = Math.min(minDp[1], minDp[2]) + x3;
+                minDp[1] = Math.min(Math.min(prevMinDp0, minDp[1]), prevMinDp2) + x2;
             }
-
         }
-        System.out.println(Arrays.stream(maxDp).max().getAsInt());
-        System.out.println(Arrays.stream(minDp).min().getAsInt());
+
+        System.out.println(
+            Math.max(maxDp[0], Math.max(maxDp[1], maxDp[2])) + " " + Math.min(minDp[0],
+                Math.min(minDp[1], minDp[2])));
     }
+
 }
+
